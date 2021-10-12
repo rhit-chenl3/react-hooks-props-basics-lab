916c80376724b334ca89bf7d85b3e2d48e038829
Leo Chen <chenl3@rose-hulman.edu>
2021-10-12 14:46:57 -0700

    commit tree bb32df4c8e6bfccb5eb6fd8e177a4e6de1334ff4

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // const isLoggedIn = props.isLoggedIn;
+  if (props.bio) {
+    return(
+      <div id="about">
+      <h2>About Me</h2>
+      <p>{props.bio}</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+    </div>
+    );
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      {/* <a href="https://github.com/liza">https://github.com/liza</a>
+      <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a> */}
     </div>
   );
 }
